a160a5a99fe6c905270f9747ec3d8ed3eae396ee
FIRST_NAME LAST_NAME <joshuadiboll@gmail.com>
2016-11-04 12:40:28 -0400

    installed bootstrap. Starting with simple binding

---
+++ src/js/exam1.js
@@ -0,0 +1,15 @@
+var app1 = angular.module('app1' []);
+
+app1.controller('controller1', function($scope){
+	$scope.first = 1;
+	$scope.second = 1;
+
+
+	$scope.updateValue=function() {
+		$scope.calculation = $scope.first + " + " + $scope.second + 
+		" = " + (+$scope.first + +$scope.second);
+	};
+
+
+
+});
