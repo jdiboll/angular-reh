64c8b6167320885089e2706abf4eb8ca1bf4f941
FIRST_NAME LAST_NAME <joshuadiboll@gmail.com>
2016-11-04 13:44:16 -0400

    simple form created with simple functionality

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,11 @@
 // Javascript Entry Point
+var app = angular.module("maxcntrl", [] );
+
+app.controller('jamie1', function($scope) {
+	$scope.formModel = {};
+	$scope.onSubmit = function() {
+		console.log("I'm submitted!");
+		console.log($scope.formModel);
+	};
+
+});
